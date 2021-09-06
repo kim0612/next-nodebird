ea4b8d0def3d5396152ade4d218064b940bd354a
kim0612 <roki03@naver.com>
2021-09-06 18:03:21 +0900

    #4.1_redux-thunk
    redux-thunk 설치하고 실습.

--- prepare/front/store/configureStore.js
+++ prepare/front/store/configureStore.js
@@ -1,11 +1,18 @@
 import { createWrapper } from "next-redux-wrapper";
 import { applyMiddleware, compose, createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension"
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunkMiddleware from 'redux-thunk';
 
 import reducer from "../reducers";
 
+// redux-thunk 예시 (간단한 로거미들웨어 제작. redux-dev-tools 같은거....)
+const loggerMiddleware = ({dispatch, getState}) => (next) => (action) => {
+  console.log(action);
+  return next(action);
+}
+
 const configureStore = () => {
-  const middlewares = []
+  const middlewares = [thunkMiddleware, loggerMiddleware];
   const enhancer = process.env.NODE_ENV === "production"
     ? compose(applyMiddleware(...middlewares))
     : composeWithDevTools(applyMiddleware(...middlewares))
