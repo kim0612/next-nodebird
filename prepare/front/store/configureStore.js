c1b2508dec65c1797fe2cf36feaeda44c7511da5
kim0612 <roki03@naver.com>
2021-09-08 21:30:21 +0900

    #4.4_rootSaga 제작
    1. redux-saga 설치.
    2. axios 설치.
    3. store에 saga미들웨어 등록하고 연동.
    4. sagas/index.js에 rootSaga 제작. (패턴만들기)

--- prepare/front/store/configureStore.js
+++ prepare/front/store/configureStore.js
@@ -1,9 +1,11 @@
 import { createWrapper } from "next-redux-wrapper";
 import { applyMiddleware, compose, createStore } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
-import thunkMiddleware from 'redux-thunk';
+// import thunkMiddleware from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
 
 import reducer from "../reducers";
+import rootSaga from '../sagas';
 
 // redux-thunk 예시 (간단한 로거미들웨어 제작. redux-dev-tools 같은거....)
 const loggerMiddleware = ({dispatch, getState}) => (next) => (action) => {
@@ -12,11 +14,13 @@ const loggerMiddleware = ({dispatch, getState}) => (next) => (action) => {
 }
 
 const configureStore = () => {
-  const middlewares = [thunkMiddleware, loggerMiddleware];
+  const sagaMiddleware = createSagaMiddleware();
+  const middlewares = [sagaMiddleware, loggerMiddleware];
   const enhancer = process.env.NODE_ENV === "production"
     ? compose(applyMiddleware(...middlewares))
     : composeWithDevTools(applyMiddleware(...middlewares))
   const store = createStore(reducer, enhancer);
+  store.sagatask = sagaMiddleware.run(rootSaga);
   return store;
 };
 
